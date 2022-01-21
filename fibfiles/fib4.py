80d7650de1647ad9232286902268f292dcf4b1e1
Thenos20 <70877429+Thenos20@users.noreply.github.com>
2022-01-21 09:04:12 +0100

    files added via pycharm repository

---
+++ fibfiles/fib4.py
@@ -0,0 +1,13 @@
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+
+def fib4(n:int) -> int:
+    if n<2:
+        return n
+    return fib4(n-2) + fib4(n-1)
+
+n = 0
+while n < 20:
+    print(fib4(n))
+    n = n + 1
