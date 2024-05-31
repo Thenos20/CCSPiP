68bcbc58e064c73f123d86df3344170af468b101
Theo Ask <70877429+Thenos20@users.noreply.github.com>
2024-05-31 15:00:40 +0200

    Add files via upload

---
+++ Chapter_2/Generic_search/Generic_search/Generic_linear.py
@@ -0,0 +1,12 @@
+def binary(sequence, key):
+    low = 0
+    high = len(sequence) - 1
+    while low <= high:
+        mid = (low + high)//2
+        if sequence[mid] < key:
+            low += mid + 1
+        elif sequence[mid] > key:
+            high = mid - 1
+        else:
+            return True
+        return False
